80b832c066b0e0be72a54e50c85160b6a0e2ea0b
Gabi_Cimadon <108761378+GabrielaCimadon@users.noreply.github.com>
2023-06-15 14:53:33 -0300

    MemoryGame

---
+++ js/login.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('.login_input');
+const button = document.querySelector('.login_button');
+const form = document.querySelector('.login-form');
+
+const validateInput = ({ target }) => {
+    if (target.value.length > 2) {
+        button.removeAttribute('disabled');
+        return;
+    }
+    button.setAttribute('disebled', '')
+}
+
+const handlesubmit = (event) => {
+    event.preventDefault();
+    
+    localStorage.setItem('player', input.value);
+    window.location ='pages/game.html';
+}
+
+input.addEventListener('input', validateInput); 
+form.addEventListener('submit', handlesubmit);
